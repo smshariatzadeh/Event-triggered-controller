f6b2c2f0746d904efd36716f409acd3d2fff6aeb
smshariatzadeh <54735737+smshariatzadeh@users.noreply.github.com>
2020-03-20 06:33:38 -0700

    Create example_et_cotinuous_LTI.m

---
+++ example_et_cotinuous_LTI.m
@@ -0,0 +1,155 @@
+% example_et_cotinuous_LTI.m
+%
+% event trigger for continuous time linear time-invariant system
+%
+% simulation is run for different event trigger parameters i.e.: in sigma(event-triggered parameter)  
+%
+% event triggered rule is based on comparing norm X with norm e
+%
+% sigma : event-triggered parameter in event rule 
+%         
+% By S.M.Shariatzadeh
+% Date :10 Mars 2020 
+
+
+%define linear time-invarient system
+sys.A= [0 1 0;
+    4 5 0;
+    1 0 1 ];
+sys.B= [0;
+    1;
+    2];
+sys.C= [1 1 1];
+
+na=size(sys.A);
+
+% designing feedback gain based on Linear-Quadratic Regulator (LQR)  
+Q=eye(3);
+R= 50;
+[K,S,e] = lqr(sys.A,sys.B,Q,R)
+
+%run simulation for different event trigger rule
+sigma_array=linspace(0,0.4,10); % 0<sigma<1
+[p,totalRun]=size(sigma_array);
+t_final=5;
+dt=0.005; %very small sample time that system is linear for it
+
+for iter = 1:totalRun
+    sigma=sigma_array(iter);
+    fprintf('step %d of %d: simulation for sigma %d:\n', iter, totalRun ,sigma);
+    
+    %initialize variable for each run
+    x=[1 2 3]'; %initial state 
+    t=0.0;
+    y=0.0;
+    yb=0.0;
+    r=0.0; % set point    
+    u=0;
+    uold=0;
+    dist=0; % disturbance    
+    n=round(t_final/dt);
+    t_array = zeros(1,n);
+    u_array = zeros(1,n);
+    event_array =  zeros(1,n);  
+    r_array =  zeros(1,n);  
+    y_array =  zeros(1,n);
+    x_array =  zeros(na(1),n);  
+    x_error_array=  zeros(1,n);
+    normX_array=  zeros(1,n);
+    snormX_array=  zeros(1,n);
+    Xnew=zeros(na(1),1);
+    Xold=zeros(na(1),1);
+    for i=1:n
+         t = t + dt;       
+         if mod(i,100)==0 
+             fprintf('\nRunning... Time: %f',t) 
+         end    
+         
+         x_error = Xnew-Xold;  
+         x_error_array(i) = abs(norm(x_error));
+         snormX_array(i)= sigma*(norm(Xnew));
+         normX_array(i)= (norm(Xnew));
+         if (sigma*(norm(Xnew))<= abs(norm(x_error)))
+             % event occured so generate new u
+             u=r-K*Xnew;
+             
+             %save data for plot curve
+             u_array(i)=u;
+             event_array(i)=1;
+             uold = u; %save u for next step
+         else
+             % event not triggered so use old u
+             u = uold; 
+             
+             %save data for plot curve
+             u_array(i)=u;
+             event_array(i)=0;
+         end
+         
+         %find system responce and new x
+         xdot = sys.A*x + sys.B*u;
+         x = x + xdot*dt;
+         Xnew=x;
+         y = sys.C*x;
+
+
+         if event_array(i)==1
+            Xold=Xnew;             
+         end    
+         
+         %save result
+         x_array(:,i)=Xnew;
+         t_array(i)=t;
+         y_array(i)=y;
+         r_array(i)=r;
+         
+    end
+
+    figure(1);
+    subplot(3,1,1)
+    plot(t_array,r_array,t_array,y_array);
+    xlabel('time(s)');ylabel('Output Value');
+    legend('y_{setpoint}','y')
+    grid on
+    subplot(3,1,2)
+    plot(t_array,u_array);
+    xlabel('time(s)');ylabel('Input Value');
+    legend('u')
+    grid on
+    subplot(3,1,3)
+    stem(t_array,event_array);
+    title('Event-triggered')
+    xlabel('time(s)');
+    
+    figure(2)
+    subplot(2,1,1)
+    plot(t_array,x_error_array,t_array, snormX_array, t_array, normX_array  );
+    legend('x_error','sigma*normX','normX')
+    
+    subplot(2,1,2)    
+    stem(t_array,event_array)
+    legend('event')
+    grid on
+
+    NumberofEvent=sum(event_array);
+    R=NumberofEvent/n;
+    R_array(iter)=R;
+    error_array(iter)=sum(abs(y_array));
+    
+    fprintf('\n\nR(Event Ratio)= %6.3f\n', R)
+    disp('------------------------')
+    disp('press any key to run another simulation')
+    pause
+end
+
+figure(5)
+subplot(2,1,1)
+set(gca,'FontSize',10);
+plot(sigma_array,R_array,'LineWidth',2,'Color','b');
+grid on
+xlabel('Sigma'); ylabel('R'); title('Event Ratio (R) vs Error Criterion (Sigma)');
+subplot(2,1,2)
+plot(sigma_array,error_array,'LineWidth',2,'Color','r');
+grid on
+xlabel('Sigma'); ylabel('abs(error)'); title(' Output Error vs Error Criterion (Sigma)');
+print('sigma_vs_R_and_E','-dpng');
